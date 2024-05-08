adff61725834d67917682e636a4113428acf610b
Rebecca Moura <rebeccamouradev@gmail.com>
2024-05-08 08:47:07 -0300

    falta responsividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './main/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HubComponent } from './main/hub/hub.component';
+import { LoginComponent } from './main/auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    HubComponent
+    HubComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main/auth/login/login.component.ts
+++ src/app/main/auth/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  isRegister: boolean = false;
 
+  toggleSignUp() {
+    this.isRegister = this.isRegister ? false : true;
+  }
 }
